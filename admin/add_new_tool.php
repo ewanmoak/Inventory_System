b32bf49e9f557e548b9396b57b9476222e4992b6
diane <dianeendaya4@gmail.com>
2024-05-20 17:17:54 +0800

    upd

--- admin/add_new_tool.php
+++ admin/add_new_tool.php
@@ -15,7 +15,6 @@ mysqli_query($db, $sql);
 $sql = "CREATE TABLE IF NOT EXISTS tools (
   id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
-  maintenance_date DATE NOT NULL,
   quantity INT(11) NOT NULL,
   description TEXT NOT NULL,
   category_id INT(11) NOT NULL,
@@ -40,14 +39,13 @@ if (mysqli_num_rows($result) > 0) {
 if (isset($_POST['submit'])) {
   // Sanitize user input (prevent SQL injection)
   $name = mysqli_real_escape_string($db, $_POST['name']);
-  $maintenance_date = mysqli_real_escape_string($db, $_POST['maintenance_date']);
   $quantity = (int)mysqli_real_escape_string($db, $_POST['quantity']); // Cast to integer
   $description = mysqli_real_escape_string($db, $_POST['description']);
   $category_id = (int)mysqli_real_escape_string($db, $_POST['category_id']); // Cast to integer
 
   // Prepare and execute insert query
   $sql = "INSERT INTO tools (unique_id, name, date_manufactured, expiration_date, maintenance_date, quantity, description, category_id, img_url)
-          VALUES ('$name', '$maintenance_date', $quantity, '$description', $category_id)";
+          VALUES ('$name', $quantity, '$description', $category_id)";
   if (mysqli_query($db, $sql)) {
     echo "<br>Tool added successfully!";
   } else {
@@ -71,8 +69,14 @@ if (isset($_POST['submit'])) {
     <label for="name">Tool Name:</label>
     <input type="text" name="name" id="name" required><br><br>
 
-    <label for="maintenance_date">Maintenance Date:</label>
-    <input type="date" name="maintenance_date" id="maintenance_date"><br><br>
+    <label for="expiration_date">Expiration Date:</label>
+    <input type="date" name="expiration_date" id="expiration_date"><br><br>
+
+    <label for="maintenance_interval">Maintenance Interval:</label>
+    <input type="date" name="maintenance_interval" id="maintenance_interval"><br><br>
+
+    <label for="last_maintained_date">Last Maintained Date:</label>
+    <input type="date" name="last_maintained_date" id="last_maintained_date"><br><br>
 
     <label for="quantity">Quantity:</label>
     <input type="text" name="quantity" id="quantity" required><br><br>
